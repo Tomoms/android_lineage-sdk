4e0ad29c8eddc3bbeea03293c47542f324e113cb
Tommaso Fonda <fondatommaso2@gmail.com>
2025-10-30 14:55:35 +0100

    Revert "Add three fingers swipe actions [1/3]"
    This reverts commit 066095c5e5a802ea5544d2d10d463de4aa16a197.

--- sdk/src/java/lineageos/providers/LineageSettings.java
+++ sdk/src/java/lineageos/providers/LineageSettings.java
@@ -1239,16 +1239,6 @@ public final class LineageSettings {
                 sHardwareKeyActionvalidator;
 
         /**
-         * Action to perform when three fingers swipe action is detected. (Default is 0)
-         * (See KEY_HOME_LONG_PRESS_ACTION for valid values)
-         */
-        public static final String KEY_THREE_FINGERS_SWIPE_ACTION = "key_three_fingers_swipe_action";
-
-        /** @hide */
-        public static final Validator KEY_THREE_FINGERS_SWIPE_ACTION_VALIDATOR =
-                sHardwareKeyActionvalidator;
-
-        /**
          * Whether to wake the screen with the home key, the value is boolean.
          * 0 = 0ff, 1 = on
          */
@@ -2171,7 +2161,6 @@ public final class LineageSettings {
             VALIDATORS.put(KEY_APP_SWITCH_LONG_PRESS_ACTION,
                     KEY_APP_SWITCH_LONG_PRESS_ACTION_VALIDATOR);
             VALIDATORS.put(KEY_EDGE_LONG_SWIPE_ACTION, KEY_EDGE_LONG_SWIPE_ACTION_VALIDATOR);
-            VALIDATORS.put(KEY_THREE_FINGERS_SWIPE_ACTION, KEY_THREE_FINGERS_SWIPE_ACTION_VALIDATOR);
             VALIDATORS.put(HOME_WAKE_SCREEN, HOME_WAKE_SCREEN_VALIDATOR);
             VALIDATORS.put(ASSIST_WAKE_SCREEN, ASSIST_WAKE_SCREEN_VALIDATOR);
             VALIDATORS.put(APP_SWITCH_WAKE_SCREEN, APP_SWITCH_WAKE_SCREEN_VALIDATOR);
