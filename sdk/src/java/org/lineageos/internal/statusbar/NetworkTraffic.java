80e294210f2c67a461b0482a32339eb835985769
grepfox <grepfox@tutamail.com>
2026-02-13 15:52:05 +0530

    sdk: keep network traffic text compact
    Change-Id: I2ad640d09333e86bc35d20976f0894ce22c9cbc8

--- sdk/src/java/org/lineageos/internal/statusbar/NetworkTraffic.java
+++ sdk/src/java/org/lineageos/internal/statusbar/NetworkTraffic.java
@@ -274,9 +274,7 @@ public class NetworkTraffic extends TextView {
                     case UNITS_MEGABYTES:
                         {
                             final String format;
-                            if (kbps < 80000) {
-                                format = "%.2f";
-                            } else if (kbps < 800000) {
+                            if (kbps < 800000) {
                                 format = "%.1f";
                             } else {
                                 format = "%.0f";
@@ -478,6 +476,10 @@ public class NetworkTraffic extends TextView {
                 LineageSettings.Secure.NETWORK_TRAFFIC_UNITS, UNITS_KILOBYTES);
         mShowUnits = LineageSettings.Secure.getInt(resolver,
                 LineageSettings.Secure.NETWORK_TRAFFIC_SHOW_UNITS, SHOW_UNITS_ON);
+        final boolean dualDirectionMode = mMode == MODE_UPSTREAM_AND_DOWNSTREAM;
+        setSingleLine(!dualDirectionMode);
+        setMaxLines(dualDirectionMode ? 2 : 1);
+        setIncludeFontPadding(false);
 
         manageNetworkCallbacks();
 
